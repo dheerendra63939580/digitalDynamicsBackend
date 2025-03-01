0935baaa34e10b023578fc52892e2933605083d5
dheerendra kumar <dheerendra16kumar@gmail.com>
2025-03-02 03:00:27 +0530

    token validation 8h

--- helper.js
+++ helper.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken")
 module.exports.tokenGenerator = (name, email) => {
-  const token = jwt.sign({name, email}, process.env.JSONWEBTOKEN_SECRET, { expiresIn: '1h' });
+  const token = jwt.sign({name, email}, process.env.JSONWEBTOKEN_SECRET, { expiresIn: '8h' });
   return token;
 }
